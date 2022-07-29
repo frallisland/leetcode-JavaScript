3fefe25c4e81d3ab5895aa5ec5bf6915a1c7203b
Andy Liu <lianchang16@qq.com>
2022-07-29 20:34:38 +0800

    20220729

---
+++ problems/0374.猜数字大小/0374-猜数字大小.js
@@ -0,0 +1,24 @@
+/** 
+ * Forward declaration of guess API.
+ * @param {number} num   your guess
+ * @return 	            -1 if num is lower than the guess number
+ *			             1 if num is higher than the guess number
+ *                       otherwise return 0
+ * var guess = function(num) {}
+ */
+
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var guessNumber = function(n) {
+    let l=1, r=n;
+    while(l<=r){
+        let m=l+((r-l)>>1);
+        // console.log(m, guess(m));
+        if(guess(m)===0) return m;
+        else if(guess(m)===1) l=m+1;
+        else r=m-1;
+    }
+    return -1;
+};
